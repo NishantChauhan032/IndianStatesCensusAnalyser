a7f7fc3ec3971b6df7310f87969e794a77de5879
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-26 00:10:47 +0530

    [NISHANT] refactor : code refactor to pass test case

---
+++ src/main/java/com/capg/StateCensusData.java
@@ -0,0 +1,19 @@
+package com.capg;
+
+
+import com.opencsv.bean.CsvBindByName;
+
+public class StateCensusData {
+
+	@CsvBindByName(column = "State")
+	private String state;
+
+	@CsvBindByName(column = "Population", required = true )
+	private int population;
+
+	@CsvBindByName(column = "AreaInSqKm")
+	private int areaInSqKm;
+
+	@CsvBindByName(column = "DensityPerSqKm")
+	private int densityPerSqKm;
+}
