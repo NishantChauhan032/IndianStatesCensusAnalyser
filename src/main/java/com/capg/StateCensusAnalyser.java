e65fff72b7459afd4dc800815a322e33c58576d2
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-25 22:22:31 +0530

    [NISHANT] add : Welcome message printed

---
+++ src/main/java/com/capg/StateCensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.capg;
+
+public class StateCensusAnalyser 
+{
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian States Census Analyser Program");
+	}
+}
